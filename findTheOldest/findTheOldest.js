74c341a897f7303db1d26ef1fe892a4a81f8c0d4
eugenegrossman <gnwgrssmn@gmail.com>
2020-04-23 19:24:25 -0700

    titles, fibonacci, oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
+let findTheOldest = function(array) {
 
+    const getAge = function(birth, death) {
+        if (!death) {
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+        }
+
+    const oldest = array.sort(function(a, b) {
+        const first = getAge(a.yearOfBirth, a.yearOfDeath)
+        const next = getAge(b.yearOfBirth, b.yearOfDeath)
+        return first > next ? -1 : 1;
+      })
+      return oldest[0]
 }
 
 module.exports = findTheOldest
+
+
